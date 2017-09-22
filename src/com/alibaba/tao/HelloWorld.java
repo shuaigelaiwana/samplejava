901263201be69cd605190cc1652f7ea1f4d0d490
ljn <ljn@ljn-PC.lan>
2017-09-22 21:47:15 +0800

    提交HelloWorld

---
+++ src/com/alibaba/tao/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.alibaba.tao;
+
+/**
+ * @author ljn
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("HelloWorld！！！");				
+	}
+
+}
