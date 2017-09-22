384f9b31eb4d3cfcdff68fee0ae78590c2b187c2
ljn <ljn@ljn-PC.lan>
2017-09-22 18:41:23 +0800

    1.项目结构初次 提交

---
+++ src/com/alibaba/tao/TestJava.java
@@ -0,0 +1,8 @@
+package com.alibaba.tao;
+
+public class TestJava {
+	
+	public void printHello() {
+		System.out.println("你好，Github");
+	}	
+}
---
+++ src/com/alibaba/tao/TestJavaRun.java
@@ -0,0 +1,10 @@
+package com.alibaba.tao;
+
+public class TestJavaRun {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+            TestJava testJava = new TestJava();
+            testJava.printHello();
+	}
+}
